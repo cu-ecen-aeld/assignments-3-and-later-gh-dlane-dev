b504aca331a37bd6616f431232bbb58b94d234e4
Darren Lane <dlane@bitbox.co.uk>
2022-12-08 19:59:41 +0000

    Add writer.c WIP

---
+++ finder-app/writer.c
@@ -0,0 +1,38 @@
+//////////////////////////////////////////////////////////////////
+// @brief - writer application to replace the writer shell script
+//////////////////////////////////////////////////////////////////
+#include <stdio.h>
+#include <syslog.h>
+
+#define WRITER_MAX_FILE_PATH_LEN (256)
+#define WRITER_MAX_STRING_LEN (256)
+
+int main(int argc, char** argv)
+{
+  int ret = 1;
+
+  openlog("writer:", 0, 0);
+  if (argc == 3 && strlen(argv[1]) <= WRITER_MAX_FILE_PATH_LEN) {
+    FILE *ptr_file = fopen(argv[1], "w");
+    unsigned int passed_string_len = strlen(argv[2]);
+
+    if (ptr_file != NULL && passed_string_len <= WRITER_MAX_STRING_LEN) {
+      if (fwrite() == passed_string_len) {
+        syslog(LOG_USER, "");
+        ret = 0;
+      } else {
+        syslog(LOG_ERR, "Failed to write string to file, %s", argv[1]);
+      }
+    } else {
+      syslog(LOG_ERR, "Failed to open file %s")
+    }
+
+  } else {
+    syslog(LOG_ERR, "Invalid arguments passed");
+  }
+
+  closelog();
+
+  return ret;
+}
+
