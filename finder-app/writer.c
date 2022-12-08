d44170138cea0907c963a1baaead007dc6ce657a
Darren Lane <dlane@bitbox.co.uk>
2022-12-08 20:39:33 +0000

    writer application WIP

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -3,6 +3,7 @@
 //////////////////////////////////////////////////////////////////
 #include <stdio.h>
 #include <syslog.h>
+#include <string.h>
 
 #define WRITER_MAX_FILE_PATH_LEN (256)
 #define WRITER_MAX_STRING_LEN (256)
@@ -15,16 +16,16 @@ int main(int argc, char** argv)
   if (argc == 3 && strlen(argv[1]) <= WRITER_MAX_FILE_PATH_LEN) {
     FILE *ptr_file = fopen(argv[1], "w");
     unsigned int passed_string_len = strlen(argv[2]);
-
+    int a ;
     if (ptr_file != NULL && passed_string_len <= WRITER_MAX_STRING_LEN) {
-      if (fwrite() == passed_string_len) {
-        syslog(LOG_USER, "");
+      if (a = fwrite(argv[2], 1, passed_string_len, ptr_file), a == passed_string_len) {
+        syslog(LOG_USER, "writing %s to %s", argv[2], argv[1]);
         ret = 0;
       } else {
-        syslog(LOG_ERR, "Failed to write string to file, %s", argv[1]);
+        syslog(LOG_ERR, "Failed to write string to file, %s, %d", argv[1], a);
       }
     } else {
-      syslog(LOG_ERR, "Failed to open file %s")
+      syslog(LOG_ERR, "Failed to open file %s", argv[1]);
     }
 
   } else {
